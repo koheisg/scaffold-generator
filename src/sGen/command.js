7fe110b61e5c53699615ded27ed03d2ab26f1ece
koheisg <koheisg@gmail.com>
2016-05-25 02:18:02 +0900

    edit prototype base

--- src/sGen/command.js
+++ src/sGen/command.js
@@ -5,29 +5,34 @@ const sGen = {};
 sGen.Command = function Command() {
   this.rails = 'bin/rails';
   this.method = 'g';
-  this.generateType = function generateType() {
-    return $('#argument option:selected').val();
-  };
-  this.createModelName = function createModelName() {
-    return $('#model_name').val();
-  };
-  this.createArgument = function createArgument() {
-    const name = [];
-    const type = [];
-    $('.name').each(function callback(i, e) {
-      name.push($(e).val());
-    });
-    $('.type').each(function callback(i, e) {
-      type.push($(e).val());
-    });
-    return name.map(function callback(num, i) {
-      const entity = ` \\\n${num}:${type[i]}`;
-      return entity;
-    }).join(' ');
-  };
-  this.create = function create() {
-    return `${this.rails} ${this.method} `
-      + `${this.generateType()} ${this.createModelName()} ${this.createArgument()}`;
-  };
 };
+
+sGen.Command.prototype.generateType = function generateType() {
+    return $('#argument option:selected').val();
+};
+
+sGen.Command.prototype.createModelName = function createModelName() {
+  return $('#model_name').val();
+};
+
+sGen.Command.prototype.createArgument = function createArgument() {
+  const name = [];
+  const type = [];
+  $('.name').each(function callback(i, e) {
+    name.push($(e).val());
+  });
+  $('.type').each(function callback(i, e) {
+    type.push($(e).val());
+  });
+  return name.map(function callback(num, i) {
+    const entity = ` \\\n${num}:${type[i]}`;
+    return entity;
+  }).join(' ');
+};
+
+sGen.Command.prototype.create = function create() {
+  return `${this.rails} ${this.method} `
+    + `${this.generateType()} ${this.createModelName()} ${this.createArgument()}`;
+};
+
 module.exports = sGen.Command;
