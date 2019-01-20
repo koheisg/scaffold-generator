75d8505817714e2b336f0ef9ce1d7a228d80659a
Kohei Sugi <kohei.sg@gmail.com>
2019-01-20 15:58:05 +0900

    Rename initialize/container

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Container from './sGen/views/initialize.jsx';
+import Container from './sGen/views/container.jsx';
 
 ReactDOM.render(
   <Container />,
