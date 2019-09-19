8529864141c411a9d6141b55ef923e5921299e03
Kohei SUGI <kohei.sg@gmail.com>
2019-09-20 01:55:56 +0900

    Fix `Invalid Host/Origin header`

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: './src/index.js',
   output: { path: path.join(__dirname, 'dist'), filename: 'bundle.js' },
+  devServer: { disableHostCheck: true },
   module: {
     rules: [
       {
